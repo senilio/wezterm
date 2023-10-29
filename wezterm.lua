94a7eaef2d247188284e2727b443d694e08478fc
senilio <emil@melt.se>
2023-10-29 22:05:00 +0100

    Add a fancy tab bar

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,33 @@ local wezterm = require("wezterm")
 local act = wezterm.action
 local c = wezterm.config_builder()
 
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local foreground = "black"
+  local main_color = wezterm.color.parse("#cccccc")
+  local edge_background = main_color:darken(0.5)
+  local background = main_color:darken(0.5)
+  if tab.is_active then
+    background = main_color
+  end
+
+  wezterm.log_warn(wezterm.gui.screens().virtual_width)
+
+  local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+  local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+  local title = "  " .. tab.tab_index + 1 .. "  "
+
+  local tab_title = {
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = foreground } },
+    { Foreground = { Color = background } },
+    { Text = SOLID_RIGHT_ARROW .. "  " },
+  }
+  return tab_title
+end)
+
 c.enable_tab_bar = true
 c.window_decorations = "TITLE|RESIZE"
 c.font = wezterm.font("Liga SFMono Nerd Font", { weight = 600 })
