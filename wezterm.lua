a1a005295dff8e50e482f285ed0b319f39537bf8
senilio <emil@melt.se>
2024-10-14 19:37:01 +0200

    Update wezterm conf

--- wezterm.lua
+++ wezterm.lua
@@ -6,19 +6,19 @@ local emptystring = ""
 local SUB_IDX = { "₁", "₂", "₃", "₄", "₅", "₆", "₇", "₈", "₉", "₁₀" }
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local title = #tab.tab_title > 0 and tab.tab_title or tab.active_pane.title
-  return {
-    { Text = SUB_IDX[tab.tab_index + 1] },
-  }
+	local title = #tab.tab_title > 0 and tab.tab_title or tab.active_pane.title
+	return {
+		{ Text = SUB_IDX[tab.tab_index + 1] },
+	}
 end)
 
 c.window_decorations = "RESIZE|INTEGRATED_BUTTONS"
 
 c.font = wezterm.font({
-  family = "Liga SFMono Nerd Font",
-  stretch = "Expanded",
-  weight = 500,
-  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	family = "Liga SFMono Nerd Font",
+	stretch = "Expanded",
+	weight = 600,
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 })
 
 c.scrollback_lines = 20000
@@ -34,7 +34,7 @@ c.show_new_tab_button_in_tab_bar = false
 c.tab_max_width = 32
 
 c.adjust_window_size_when_changing_font_size = false
-c.skip_close_confirmation_for_processes_named = {}
+c.skip_close_confirmation_for_processes_named = { "fish" }
 c.native_macos_fullscreen_mode = false
 c.disable_default_key_bindings = false
 
@@ -42,106 +42,106 @@ c.selection_word_boundary = " \t\n{}[]()<>\"'`,;=^"
 
 c.mouse_wheel_scrolls_tabs = false
 c.mouse_bindings = {
-  {
-    event = { Up = { streak = 1, button = "Left" } },
-    mods = "NONE",
-    action = act.CompleteSelection("ClipboardAndPrimarySelection"),
-  },
-  {
-    event = { Up = { streak = 1, button = "Left" } },
-    mods = "CMD",
-    action = act.OpenLinkAtMouseCursor,
-  },
-  {
-    event = { Down = { streak = 1, button = "Left" } },
-    mods = "CTRL",
-    action = act.Nop,
-  },
-  {
-    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
-    mods = "NONE",
-    action = act.ScrollByCurrentEventWheelDelta,
-  },
-  {
-    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
-    mods = "NONE",
-    action = act.ScrollByCurrentEventWheelDelta,
-  },
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.CompleteSelection("ClipboardAndPrimarySelection"),
+	},
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CMD",
+		action = act.OpenLinkAtMouseCursor,
+	},
+	{
+		event = { Down = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.Nop,
+	},
+	{
+		event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+		mods = "NONE",
+		action = act.ScrollByCurrentEventWheelDelta,
+	},
+	{
+		event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+		mods = "NONE",
+		action = act.ScrollByCurrentEventWheelDelta,
+	},
 }
 
 c.hyperlink_rules = { { regex = "\\b\\w+://[\\w.-]+\\.[a-z]+[\\w/#\\.-]+\\b", format = "$0" } }
 
 c.keys = {
-  -- Misc
-  { key = "Enter", mods = "CMD", action = act.ToggleFullScreen },
+	-- Misc
+	{ key = "Enter", mods = "CMD", action = act.ToggleFullScreen },
 
-  -- Splits
-  { key = "d", mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-  { key = "d", mods = "CMD|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-  { key = "w", mods = "CMD", action = act.CloseCurrentPane({ confirm = true }) },
-  -- Navigate panes
-  { key = "LeftArrow", mods = "ALT|CMD", action = act.ActivatePaneDirection("Left") },
-  { key = "RightArrow", mods = "ALT|CMD", action = act.ActivatePaneDirection("Right") },
-  { key = "UpArrow", mods = "ALT|CMD", action = act.ActivatePaneDirection("Up") },
-  { key = "DownArrow", mods = "ALT|CMD", action = act.ActivatePaneDirection("Down") },
+	-- Splits
+	{ key = "d", mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "d", mods = "CMD|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "w", mods = "CMD", action = act.CloseCurrentPane({ confirm = true }) },
+	-- Navigate panes
+	{ key = "LeftArrow", mods = "ALT|CMD", action = act.ActivatePaneDirection("Left") },
+	{ key = "RightArrow", mods = "ALT|CMD", action = act.ActivatePaneDirection("Right") },
+	{ key = "UpArrow", mods = "ALT|CMD", action = act.ActivatePaneDirection("Up") },
+	{ key = "DownArrow", mods = "ALT|CMD", action = act.ActivatePaneDirection("Down") },
 
-  -- Move Tabs
-  { key = "LeftArrow", mods = "SHIFT|CTRL", action = act.MoveTabRelative(-1) },
-  { key = "RightArrow", mods = "SHIFT|CTRL", action = act.MoveTabRelative(1) },
+	-- Move Tabs
+	{ key = "LeftArrow", mods = "SHIFT|CTRL", action = act.MoveTabRelative(-1) },
+	{ key = "RightArrow", mods = "SHIFT|CTRL", action = act.MoveTabRelative(1) },
 
-  -- Organize panes
-  { key = "LeftArrow", mods = "SHIFT|CMD", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
-  { key = "RightArrow", mods = "SHIFT|CMD", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
-  { key = "UpArrow", mods = "SHIFT|CMD", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
-  { key = "DownArrow", mods = "SHIFT|CMD", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
+	-- Organize panes
+	{ key = "LeftArrow", mods = "SHIFT|CMD", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
+	{ key = "RightArrow", mods = "SHIFT|CMD", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
+	{ key = "UpArrow", mods = "SHIFT|CMD", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
+	{ key = "DownArrow", mods = "SHIFT|CMD", action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
 
-  -- Resize panes
-  { key = "LeftArrow", mods = "CMD", action = act.AdjustPaneSize({ "Left", 15 }) },
-  { key = "RightArrow", mods = "CMD", action = act.AdjustPaneSize({ "Right", 15 }) },
-  { key = "UpArrow", mods = "CMD", action = act.AdjustPaneSize({ "Up", 5 }) },
-  { key = "DownArrow", mods = "CMD", action = act.AdjustPaneSize({ "Down", 5 }) },
+	-- Resize panes
+	{ key = "LeftArrow", mods = "CMD", action = act.AdjustPaneSize({ "Left", 15 }) },
+	{ key = "RightArrow", mods = "CMD", action = act.AdjustPaneSize({ "Right", 15 }) },
+	{ key = "UpArrow", mods = "CMD", action = act.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "DownArrow", mods = "CMD", action = act.AdjustPaneSize({ "Down", 5 }) },
 }
 c.send_composed_key_when_left_alt_is_pressed = true
 
 c.color_scheme = "Hej"
 c.color_schemes = {
-  ["Hej"] = {
-    tab_bar = { background = "#111111" },
-    foreground = "#ffffff",
-    background = "#000015",
-    cursor_bg = "#dadada",
-    ansi = { "#000000", "#ff4b2f", "#3ac33a", "#c7c400", "#4761da", "#ba57b8", "#00c5c7", "#c7c7c7" },
-    brights = { "#676767", "#ff6d67", "#5ff967", "#fefb67", "#6871ff", "#ff76ff", "#5ffdff", "#fffefe" },
-  },
+	["Hej"] = {
+		tab_bar = { background = "#111111" },
+		foreground = "#ffffff",
+		background = "#000000",
+		cursor_bg = "#dadada",
+		ansi = { "#000000", "#ff4b2f", "#3ac33a", "#c7c400", "#4761da", "#ba57b8", "#00c5c7", "#c7c7c7" },
+		brights = { "#676767", "#ff6d67", "#5ff967", "#fefb67", "#6871ff", "#ff76ff", "#5ffdff", "#fffefe" },
+	},
 }
 c.cursor_thickness = "5px"
 c.colors = {
-  selection_fg = "#222222",
-  selection_bg = "#aaaaaa",
-  cursor_border = "#aaaaaa",
+	selection_fg = "#222222",
+	selection_bg = "#aaaaaa",
+	cursor_border = "#aaaaaa",
 
-  tab_bar = {
-    inactive_tab_edge = "#000000",
-    active_tab = { bg_color = "#121212", fg_color = "#555555" },
-    inactive_tab = { bg_color = "#0e0e0e", fg_color = "#272727" },
-  },
+	tab_bar = {
+		inactive_tab_edge = "#000000",
+		active_tab = { bg_color = "#121212", fg_color = "#555555" },
+		inactive_tab = { bg_color = "#0e0e0e", fg_color = "#272727" },
+	},
 }
 
 c.use_resize_increments = true
 c.window_padding = { left = 4, right = 1, top = 3, bottom = 4 }
 c.window_frame = {
-  font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-  font_size = 11.0,
-  active_titlebar_bg = "#1a1a1a",
-  inactive_titlebar_bg = "#222222",
-  border_left_width = "1px",
-  border_right_width = "1px",
-  border_bottom_height = "1px",
-  border_top_height = "1px",
-  border_left_color = "#333333",
-  border_right_color = "#333333",
-  border_bottom_color = "#333333",
-  border_top_color = "#333333",
+	font = wezterm.font({ family = "SFMono Nerd Font", weight = "Light" }),
+	font_size = 11.0,
+	active_titlebar_bg = "#0c0c0c",
+	inactive_titlebar_bg = "#222222",
+	border_left_width = "1px",
+	border_right_width = "1px",
+	border_bottom_height = "1px",
+	border_top_height = "1px",
+	border_left_color = "#333333",
+	border_right_color = "#333333",
+	border_bottom_color = "#333333",
+	border_top_color = "#333333",
 }
 
 c.inactive_pane_hsb = { saturation = 1.0, brightness = 0.7 }
